5b15be190797df52e2d0a015d0c296dd09daf081
umamaheshrao <96564185+umamaheshrao@users.noreply.github.com>
2022-01-07 12:00:29 +0530

    Add files via upload

---
+++ c++.cpp
@@ -0,0 +1,9 @@
+/*A C progremmer to perform adding*/
+#include<studio.h.
+int main()
+{
+int a,b,c;
+printf("Enter any two values");
+scanf("%d%d,"&a,&b);
+c=a+b;
+printf("the addition of %d and %d is %d",a,b,c)
